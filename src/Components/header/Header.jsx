82a800ee7e70dd3b1b397bd2af1405fd306d6873
abhishek-kumar-05 <kumarabhi0513@gmail.com>
2024-11-06 19:24:06 +0530

    created header component

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <h1 className="app-title">Image Reducer</h1>
+    </div>
+  );
+};
+
+export default Header;
